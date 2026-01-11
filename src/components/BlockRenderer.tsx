1a131a3ec24297c163334855ceb673e1c3b3bb0b
Michael Gutensohn <michael@gutensohn.me>
2026-01-10 20:12:04 -0800

    feat: add BlockRenderer to delegate to block type components

---
+++ src/components/BlockRenderer.tsx
@@ -0,0 +1,41 @@
+import type { Block as BlockType } from '../lib/blockTypes';
+import { HeadingBlock } from './blocks/HeadingBlock';
+import { TaskBlock } from './blocks/TaskBlock';
+import { ListBlock } from './blocks/ListBlock';
+import { CodeBlock } from './blocks/CodeBlock';
+import { ParagraphBlock } from './blocks/ParagraphBlock';
+
+interface BlockRendererProps {
+  block: BlockType;
+  onActivate: () => void;
+  onToggleTask?: () => void;
+}
+
+export function BlockRenderer({ block, onActivate, onToggleTask }: BlockRendererProps) {
+  switch (block.type) {
+    case 'heading1':
+    case 'heading2':
+    case 'heading3':
+      return <HeadingBlock content={block.content} type={block.type} onClick={onActivate} />;
+
+    case 'task':
+      return (
+        <TaskBlock
+          content={block.content}
+          onClick={onActivate}
+          onToggle={onToggleTask || (() => {})}
+        />
+      );
+
+    case 'bulletList':
+    case 'numberedList':
+      return <ListBlock content={block.content} type={block.type} onClick={onActivate} />;
+
+    case 'code':
+      return <CodeBlock content={block.content} onClick={onActivate} />;
+
+    case 'paragraph':
+    default:
+      return <ParagraphBlock content={block.content} onClick={onActivate} />;
+  }
+}

616f4fd393eaf9b8cc8c8a5e382c38548abd44a1
Michael Gutensohn <michael@gutensohn.me>
2026-01-10 20:10:46 -0800

    feat: add TaskBlock with clickable checkbox

---
+++ src/components/blocks/TaskBlock.tsx
@@ -0,0 +1,29 @@
+interface TaskBlockProps {
+  content: string;
+  onClick: () => void;
+  onToggle: () => void;
+}
+
+export function TaskBlock({ content, onClick, onToggle }: TaskBlockProps) {
+  const isCompleted = /^- \[x\]/i.test(content);
+  const taskText = content.replace(/^- \[(x| )\] /i, '');
+
+  const handleCheckboxClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    onToggle();
+  };
+
+  return (
+    <div onClick={onClick} className="flex gap-2 cursor-pointer">
+      <input
+        type="checkbox"
+        checked={isCompleted}
+        onChange={handleCheckboxClick}
+        className="mt-1 cursor-pointer"
+      />
+      <span className={isCompleted ? 'line-through' : ''}>
+        {taskText}
+      </span>
+    </div>
+  );
+}

1aba4a168bd5094099170d4f9b14e7bc67f9041a
Michael Gutensohn <michael@gutensohn.me>
2026-01-23 14:08:41 -0800

    fixed padding on iOS PWA.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,15 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <main className="p-4 min-h-screen flex flex-row justify-center">
+    <main
+      className="p-4 min-h-screen flex flex-row justify-center"
+      style={{
+        paddingTop: 'max(1rem, var(--safe-area-inset-top, 0px))',
+        paddingRight: 'max(1rem, var(--safe-area-inset-right, 0px))',
+        paddingBottom: 'max(1rem, var(--safe-area-inset-bottom, 0px))',
+        paddingLeft: 'max(1rem, var(--safe-area-inset-left, 0px))',
+      }}
+    >
       {children}
       <CommandButton />
     </main>
